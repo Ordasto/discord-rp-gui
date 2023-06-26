84b59c35e5dbf247c0390726e76e8548ccde4342
Sam Peters <sam.peters24@gmail.com>
2023-06-26 21:57:28 +0100

    readme, default for activity, ipc close stuff

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Discord Custom Rich Presence with Rust and Tauri GUI
+
+## TODO
+### High priority
+- [] Complete GUI
+- [] 
+### Medium Priority
+- [] Allow custom discord app ids to be entered to allow custom images and stuff
+- [] Tray minimisation stuff
+### Low priority
+- [] Manage all errors instead of just unwrap() -ing everything like a bozo
+- [] text has to be atleast 2 characters (2 spaces works) for it to update. Have it check for this and add spaces to make sure it hits this req
+- [] large/small image has to be accompanied by large/small text or it wont update. Have it also check for this and add spaces if needed (Need to test this again)
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,72 +2,125 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use discord_rich_presence::{
-    activity::{self, Activity, Assets, Button},
+    activity::{self, Activity, Assets, Button, Timestamps, Party},
     DiscordIpc, DiscordIpcClient,
 };
+use serde::Serialize;
 use std::{
-    sync::{Mutex, mpsc::{channel, Receiver, Sender}, self},
+    fmt::format,
+    sync::{
+        self,
+        mpsc::{channel, Receiver, Sender},
+        Mutex,
+    },
     thread::{self, JoinHandle},
-    time::Duration, fmt::format,
+    time::Duration,
 };
 use tauri::State;
 
-static mut sender: Option<Sender<ActivityData>> = None;
+
+
+static mut SENDER: Option<Sender<ActivityData>> = None;
 
 struct DiscordState {
     discord_activity: Mutex<ActivityData>,
     handle: JoinHandle<()>,
 }
 
-// Implement serde::Serialize (maybe Deserialize) so i can pass it directly from javascript
-// Although that might only apply to data passed to js, i wan't to pass data from js
-#[derive(Clone)]
-struct ActivityData {
-    state_msg: String,
-    details: String,
+#[derive(Clone,Serialize)]
+struct Image{
+    image_id:String,
+    image_label:String,
 }
+
+// Implement serde::Serialize (maybe Deserialize) so i can pass it directly from javascript
+// Although that might only apply to data passed to js, i want to pass data from js
+#[derive(Clone, Serialize)]
+struct ActivityData {
+    kill_app: bool,
+    state: String,
+    details: String,
+    timestamps: Option<Timestamps>,
+    party: Option<(String, i32)>,
+    large_image: Image,
+    small_image: Image,
+    button_one: Option<(String, String)>, // Just using a tuple for now i cant be bothered
+    button_two: Option<(String, String)>,
+    // not doing secrets right now, cba
+    // secrets: 
+
+}
+
+impl ActivityData {
+    fn new() -> Self {
+        // defualt empty string of two spaces
+        // discord doesn't accept activities with empty or 1 char strings
+        let def_str = "  ".to_string();
+        return ActivityData {
+            kill_app: false,
+            state: def_str.clone(),
+            details: def_str.clone(),
+            timestamps: None,
+            party: None,
+            large_image: Image{image_id: def_str.clone(), image_label: def_str.clone()},
+            small_image: Image{image_id: def_str.clone(), image_label: def_str.clone()},
+            button_one: None,
+            button_two: None,
+        };
+    }
+}
+
 
 #[tauri::command]
 fn update_status(stateMsg: &str, detailsMsg: &str, mut state: State<DiscordState>) -> String {
-    let mut data = ActivityData {
-        state_msg: stateMsg.to_string(),
-        details: detailsMsg.to_string(),
-    };
+    let mut data = ActivityData::new();
+    data.state = stateMsg.to_string();
+    data.details = detailsMsg.to_string();
+
     state.handle.thread().unpark();
-    unsafe{
-        sender.clone().unwrap().send(data).unwrap();
+    unsafe {
+        SENDER.clone().unwrap().send(data).unwrap();
     }
 
     return format!("no fucking idea");
 }
 
 fn main() {
-
-    let (tx,rx) = channel();
+    let (tx, rx) = channel();
 
     unsafe {
-        sender = Some(tx);
+        SENDER = Some(tx);
     }
 
-    let discord_handle = thread::spawn(|| {discord_init(rx)});
+    let discord_handle = thread::spawn(|| discord_init(rx));
     tauri::Builder::default()
-        .manage(
-            DiscordState {
-                discord_activity: ActivityData {
-                    state_msg: String::new(),
-                    details: String::new(),
-                }
-                .into(),
-                handle: discord_handle,
+        .manage(DiscordState {
+            discord_activity: ActivityData::new().into(),
+            handle: discord_handle,
+        })
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                // TELL DISCORD RP TO CLOSE CONNECTIION
+
+                // this doesn't work
+                // handle.unpark();
+                // unsafe {
+                //     SENDER.clone().unwrap().send(ActivityData {
+                //         kill_app: true,
+                //         state_msg: "".to_string(),
+                //         details: "".to_string(),
+                //     }).unwrap();
+                // }
+            }
+            _ => {}
         })
         .invoke_handler(tauri::generate_handler![update_status])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
-fn discord_init(rx: Receiver<ActivityData>){
-
-    let mut client = DiscordIpcClient::new("1122014998231781407").unwrap();
+fn discord_init(rx: Receiver<ActivityData>) {
+    let mut client = DiscordIpcClient::new("1122968332698656870").unwrap();
 
     client.connect().unwrap();
 
@@ -88,27 +141,36 @@ fn discord_init(rx: Receiver<ActivityData>){
         .small_text("Huh?");
 
     let activity = activity::Activity::new()
-        .state("Multi threading will be the end of me") // I know this is changed before being displayed, it was just to test the ability to change things after
+        .state("Multi threading will be the end of me")
         .details("just testing some stuff please")
         .buttons(buttons)
         .assets(assets);
 
     client.set_activity(activity.clone()).unwrap();
-    
+
     // have a loop that parks the thread, then when it is externally unparked, modify activity, set_activity then park again
-    
+
     loop {
         thread::park();
+
         let send_data = rx.recv().unwrap();
 
-        let new_activity = activity.clone()
-        .details(&send_data.details)
-        .state(&send_data.state_msg)
-        ;
+        if send_data.kill_app {
+            client.clear_activity().unwrap();
+            client.close().unwrap();
+            break;
+        }
 
+        let assets = Assets::new().large_image("sadge2").large_text("test");
+
+        let new_activity = activity
+            .clone()
+            .details(&send_data.details)
+            .state(&send_data.state)
+            .assets(assets);
+
+        // let new_activity = new_activity.buttons(vec![]);
 
         client.set_activity(new_activity).unwrap();
-        // Do something with the data.
-
     }
 }
